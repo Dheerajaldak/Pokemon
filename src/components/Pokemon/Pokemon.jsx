4c5976a2f8819086fd693e2cb235da073cfdc974
unknown <VISYS@VISYS-HYD16.visys.net>
2025-02-28 10:35:37 +0530

    first pokemon commit

---
+++ src/components/Pokemon/Pokemon.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Pokemon.css"
+const Pokemon = ({name, image}) => {
+  return (
+    <div className='pokemon'>
+        
+        <div className='pokemon-name'>{name}</div>
+        <div><img className='pokemon-image' src={image} alt={name} /></div>
+    </div>
+  )
+}
+
+export default Pokemon
